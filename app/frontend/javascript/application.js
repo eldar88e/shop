2610466aca7e6cc0f70541f74dfa2f3d5c3c28f0
Eldar Eminov <em88el@gmail.com>
2025-03-15 07:55:25 +0300

    Build(gem): add slim

---
+++ app/frontend/javascript/application.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        // document.body.classList.add("bg-orange-900");
+        console.log('worked!');
+    }, 3000);
+});
--- app/frontend/javascript/controllers/index.js
+++ app/frontend/javascript/controllers/index.js
@@ -4,32 +4,5 @@
 
 import { application } from "./application"
 
-import AgreementController from "./application/agreement_controller"
-application.register("agreement", AgreementController)
-
-import DadataController from "./application/dadata_controller"
-application.register("dadata", DadataController)
-
-import ButtonsController from "./application/buttons_controller"
-application.register("buttons", ButtonsController)
-
-import PhoneMaskController from "./application/phone_mask_controller"
-application.register("phone_mask", PhoneMaskController)
-
-import NoticesController from "./application/notices_controller"
-application.register("notices", NoticesController)
-
-import RatingController from "./application/rating_controller"
-application.register("rating", RatingController)
-
-import MenuController from "./application/menu_controller"
-application.register("menu", MenuController)
-
-import CartController from "./application/cart_controller"
-application.register("cart", CartController)
-
-import OpenCartController from "./application/open_cart_controller"
-application.register("open-cart", OpenCartController)
-
-import VibrationController from "./application/vibration_controller"
-application.register("vibration", VibrationController)
+// import AgreementController from "./agreement_controller"
+// application.register("agreement", AgreementController)
