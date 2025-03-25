0079e44cbe39d9ce3b7273ab1920d49d06c2e9de
Eldar Eminov <em88el@gmail.com>
2025-03-26 02:49:41 +0300

    Fix(controller): add skip_forgery_protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include CableReady::Broadcaster
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  before_action :current_cart
+  skip_forgery_protection # for test
   helper_method :current_cart
 
   private
